cfd29cde36b5969129d4d5e578991c23315417ea
Salvador Jesús Romero Castellano <salvadorjesus@gmail.com>
2023-09-12 19:56:53 +0200

    Added documentation and a couple of TODO to DemoDonable.sol.

--- DemoDonable.sol
+++ DemoDonable.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.8.15;
 
 import "./Donable.sol";
 
+/**
+* @title DemoDonable
+* @notice  A demo contract that inherits from Donable and make use of its
+* functions to accept donations.
+*/
 contract DemoDonable is Donable
 {
+    /**
+    * @notice Keep track of the money the contract has charge to its users.
+    */
     uint public contractMoney;
 
+    /**
+    * @notice Emulates doing charging the user for 1000 Wei and keeping
+    * the rest of money transferred in the transaction as a donation.
+    * @dev Uses the Donable function keepTheChange().
+    */
     function doSomethingFor1000WeiA() public payable
     {
         require(msg.value >= 1000, "Not enough WEI. Please send 1000 or more");
@@ -14,6 +27,11 @@ contract DemoDonable is Donable
         keepTheChange(1000);
     }
 
+    /**
+    * @notice Emulates doing charging the user for 1000 Wei and keeping
+    * the rest of money transferred in the transaction as a donation.
+    * @dev Uses the Donable function donateAmount(uint donation).
+    */
     function doSomethingFor1000WeiB() public payable
     {
         require(msg.value >= 1000, "Not enough WEI. Please send 1000 or more");
@@ -21,4 +39,7 @@ contract DemoDonable is Donable
         uint donation = msg.value - 1000;
         donateAmount(donation);
     }
+
+    //TODO a function to retire all contract funds.
+    //TODO a function to retire all contract funds minus donations.
 }
