497c812fb1a859d308b3e10be2bc8edd46e1d69b
salvadorjesus <salvadorjesus@gmail.com>
2023-09-12 16:53:09 +0200

    Added comments to contracts.

--- Donable.sol
+++ Donable.sol
@@ -1,18 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-/**
- * @title Donable
- * @dev Provide functionality for a contract to receive donations.
- */
 import "./Ownable.sol";
 
+/**
+ * @title Donable
+ * @dev Provide functionality for a contract to receive and keep track of donations.
+ * The contract user can directly donate to the contract using the public function donate,
+ * or any function of a child contract can keep what’s was not used from msg.amount as a
+ * donation using the modifier keepTheChange.
+ */
 contract Donable is Ownable
 {
+    /** @dev Keeps track of the amount of WEI stored in the contract that is a donation.
+    */
     uint private donationPot;
 
+    /**
+    * @dev Emitted when a donation is made. Indexed by donor.
+    */
     event DonationMade(address indexed donor, uint donation);
 
+    /**
+    * @dev Modifier for a payable function of a child contract to accept
+    * implicit donations. All WEI sent msg.amount not used in the function can
+    * be stored in the change variable by the developer, and the contract will
+    * keep track of said amount as a donation.
+    */
     modifier keepTheChange ()
     {
         uint change;
@@ -21,25 +35,28 @@ contract Donable is Ownable
     }
 
     /**
-     * @dev Send all stored donation to the contract owner.
+     * @notice Send all stored donations to the contract owner.
      */
     function claimDonations() public requireOwner
     {
         address payable to = payable(super.getOwner());
         //Checks - effect - interaction pattern
+        //TODO check for correct amount available.
         uint donationsToSend = donationPot;
         donationPot = 0;
         to.transfer(donationsToSend);
     }
 
+    /**
+    * @notice Use this function to make a donation to the contract owner.
+    */
     function donate() public payable
     {
-        donationPot += msg.value;
-        emit DonationMade(msg.sender, msg.value);
+        donateAmount(msg.value);
     }
 
     function donateAmount(uint donation) internal
-    {
+    {        
         if (donation > 0)
         {
             donationPot += donation;
--- Ownable.sol
+++ Ownable.sol
@@ -3,16 +3,23 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * @title Ownable
- * @dev Set & change owner
+ * @dev Set & change owner. A modification of the sample contract on Remix.
  */
 contract Ownable {
 
+    /**
+    * @dev Storage variable to keep track of the contract owner.
+    */
     address private owner;
     
-    // event for EVM logging
+    /**
+    * @dev Event for EVM logging. Emitted during owner change or contract creation.
+    */ 
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
     
-    // modifier to check if caller is owner
+    /**
+    * @dev Modifier to check if the function caller is owner
+    */
     modifier requireOwner() {
         // If the first argument of 'require' evaluates to 'false', execution terminates and all
         // changes to the state and to Ether balances are reverted.
@@ -21,7 +28,7 @@ contract Ownable {
     }
     
     /**
-     * @dev Set contract deployer as owner
+     * @dev Constructor. Sets the contract deployer as owner. 
      */
     constructor() {
         owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
